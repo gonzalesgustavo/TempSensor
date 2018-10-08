7f7709b348c3127eb871ddd27d3c1327fe918271
gustavogonzales <gcodingconnected@outlook.com>
2018-10-07 18:08:40 -0600

    commented code for submission

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+//required statments
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
 const SerialPort = require('serialport')
 const email = require('emailjs')
 
+//select web port
+const webPort =  3000
+
+//initialize express
 const app = express()
 
+//arduino setup (set port for the arduino you have)
 const arduinoPort = 'yourport'
+//set the baud rate
 const baudRate = 9600
 
 //sent email settings
@@ -25,36 +32,40 @@ const server = email.server.connect({
    ssl:     true
 })
 
-
+//setup for Serial port to parse the information comming in
 const Readline = SerialPort.parsers.Readline
 const ports = new SerialPort(arduinoPort, {
   baudRate: baudRate
 })
-
+//read lines from the serial port
 const parser = new Readline()
 ports.pipe(parser)
 
+//setup view and template should it be needed
 app.set('view engine', 'ejs')
+//setup folder views to handle views
 app.set('views', path.join(__dirname, 'views'))
-
+//setup body poarser to handle form submission
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
-
+//setup the public folder
 app.use(express.static(path.join(__dirname, 'public')))
 
+//home route
 app.get('/', (req, res)=>{
 	res.render('index', {
 		title: 'Plant Safe ?'
 	})
 })
-
+//get sensore data via post
 app.get('/arduinodata', (req, res, next)=>{
 	parser.on('data', (data)=>{
 		res.end(data)
 	})
 })
-
+//email when temperature or light is too high
 app.post('/emailwarn', (req, res, next)=>{
+	//use send mail to notify me
 		server.send({
 		   text:    req.body.message, 
 		   from:    from, 
@@ -63,6 +74,6 @@ app.post('/emailwarn', (req, res, next)=>{
 		}, function(err, message) { console.log(err || message); });
 })
 
-app.listen(3000, ()=>{
+app.listen(webPort, ()=>{
 	console.log("Server started on port 3000")
 })
--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,22 @@
 setInterval(()=>{
+	//get data and parse json, passing it to the view
 	$.get('/arduinodata', (data)=>{
 		let dats = JSON.parse(data)
 		$('.temps').text(dats[0].temp)
 		$('.humds').text(dats[0].humid)
 		$('.sun').text(dats[0].sun)
 		$('.lgt').text(dats[0].hif)
-		if(dats[0].temp > 75.0 || dats[0].sun > 850){
+		if(dats[0].temp > 75.0 || dats[0].sun > 850 || dats[0].temp < 62.0 ){
 			alert_user(dats[0].temp, dats[0].humid, dats[0].sun)
 		}
   	})
 }, 5202)
 
-
+//alert user function 
 function alert_user(temp, humid, light){
+	//get date and time
 	let time = dateTime()
+	//ajax post request to the backend when the sensor data is above requirements
 	$.ajax({
 		url: "/emailwarn",
 		method: "POST",
@@ -22,6 +25,7 @@ function alert_user(temp, humid, light){
 	})
 }/*end of alert_user*/
 
+//get the current date and time
 function dateTime(){
 	const dayList = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 	const date = new Date()
@@ -37,4 +41,4 @@ function dateTime(){
 	}
 	return `${dayList[date.getDay() + 1]}, ${hour}:${min}:${sec} ${mode}`
 
-}
+}/*end of dateTime*/
